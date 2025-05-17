470870c9d17057dd4833bdaff861af778a07126c
Sayanti-Roy <2004roysayanti@gmail.com>
2025-05-17 13:32:34 +0530

    Add files via upload

---
+++ Topic 1/name_greating.py
@@ -0,0 +1,1 @@
+print("Hello, my name is Sayanti! Nice to meet you.")
---
+++ Topic 1/script with comments.py
@@ -0,0 +1,5 @@
+# This script prints a greeting message
+name = "Sayanti"  # Storing the name in a variable
+greeting = "Hello and welcome!"  # Greeting message
+print(name)  # Print the name
+print(greeting)  # Print the greeting
